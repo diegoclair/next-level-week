b727abeb470d20f767a57ef4f96817625a3e9023
Diego Clair <diego93rodrigues@gmail.com>
2020-06-06 18:59:30 -0300

    Initial sctructure for mobile app

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import Constants from 'expo-constants'
+import {Feather as Icon} from '@expo/vector-icons'
+import { StyleSheet, View, Text } from 'react-native';
+import {useNavigation} from '@react-navigation/native'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import MapView from 'react-native-maps'
+import {SvgUri} from 'react-native-svg'
+
+// to install maps -> expo install react-native-maps
+
+const Points = () => {
+  const {manifest} = Constants;
+  const uri = `http://${manifest.debuggerHost?.split(':').shift()}:3333/uploads/baterias.svg`;
+
+
+  const navigation = useNavigation();
+
+  function handleNavigateBack(){
+    navigation.goBack();
+  }
+  
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name='arrow-left' size={20} color='#34cb79'/>
+        </TouchableOpacity>
+        
+        <Text style={styles.title}>Bem vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          <MapView style={styles.map}></MapView>
+        </View>
+      </View>
+
+      <View style={styles.itemsContainer}>
+        <TouchableOpacity style={styles.item} onPress={() => {}}>
+          <SvgUri width={42} height={42} uri={uri}/>
+          <Text style={styles.itemTitle}>Lâmpadas</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.item} onPress={() => {}}>
+          <SvgUri width={42} height={42} uri={uri}/>
+          <Text style={styles.itemTitle}>Lâmpadas</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.item} onPress={() => {}}>
+          <SvgUri width={42} height={42} uri={uri}/>
+          <Text style={styles.itemTitle}>Lâmpadas</Text>
+        </TouchableOpacity>
+
+      </View>
+
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20 + Constants.statusBarHeight,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80, 
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
+
+export default Points;
