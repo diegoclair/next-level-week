ff410f43d8a5ddd148262f30b3cefb37faa25d76
Diego Clair <diego93rodrigues@gmail.com>
2020-06-07 11:03:41 -0300

    Added validation fields at point create router

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from 'multer';
+import {celebrate, Joi} from 'celebrate'
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -14,8 +15,26 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 
 //index for all registers and show for only one register
-routes.post('/points', upload.single('image'), pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+
+routes.post('/points', 
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false, //without this, when it found a first error it return, with this abortEarly, it validate all fields and return all errors
+  }),
+  pointsController.create
+  );
 
 export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import routes from './routes/routes'
 import path from 'path'
 import cors from 'cors'
+import {errors} from 'celebrate'
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.use(routes);
 
 //function to provide static files
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
